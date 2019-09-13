a9626f88213282997304c2e6d237414947bfbcbe
schegol <48925789+schegol@users.noreply.github.com>
2019-09-13 22:50:31 +0300

    Верстка блока карты

---
+++ source/js/script.js
@@ -0,0 +1,33 @@
+//map://
+
+var mapInteractive = document.querySelector('#map');
+if (mapInteractive) {
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+      center: [59.932941, 30.327253],
+      zoom: 16,
+      controls: []
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+
+    myPlacemarkWithContent = new ymaps.Placemark([59.932707, 30.327213], {
+      hintContent: 'ул. Думская, д. 9',
+      balloonContent: 'Cafe Glitch'
+    }, {
+      iconLayout: 'default#imageWithContent',
+      iconImageHref: 'img/icon-marker-inverted.svg',
+      iconImageSize: [60, 90],
+      iconImageOffset: [-27, -95],
+      iconContentOffset: [0, 0],
+      iconContentLayout: MyIconContentLayout
+    });
+
+    myMap.geoObjects.add(myPlacemarkWithContent);
+    myMap.controls.add('zoomControl');
+  });
+}
